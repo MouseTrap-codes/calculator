8d2413129f1274bd1dca8d91542755ca30cdde8f
MouseTrap-codes <mousetrapcodes@gmail.com>
2025-06-21 00:34:21 -0500

    Create logic for arithmetic
    We have functions for add subtract multiply divide,
    and we call each one based on the operator entered
    in the operate() function.

---
+++ scripts.js
@@ -0,0 +1,34 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b !== 0) {
+        return a / b;
+    } else {
+        return "ERROR";
+    }
+}
+
+function operate(a, b, operator) {
+    let num1 = parseFloat(a);
+    let num2 = parseFloat(b);
+
+    if (operator === "+") {
+        return add(num1, num2);
+    } else if (operator === "-") {
+        return subtract(num1, num2);
+    } else if (operator === "x") {
+        return multiply(num1, num2);
+    } else if (operator === "/") {
+        return divide(num1, num2);
+    }
+}
