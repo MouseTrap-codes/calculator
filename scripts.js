06bc1bd1c0d094c6cb9f80f46383b7578dfefccc
MouseTrap-codes <mousetrapcodes@gmail.com>
2025-06-21 16:19:51 -0500

    Complete writing logic for calculator
    The calculator now handles arithmetic, clearing,
    deleting, decimal points, and calculation chaining
    correctly.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,30 @@
+// clear the display
+const display = document.querySelector(".display");
+display.textContent = "";
+
+// object to store relevant fields for operations
+const calculator = {
+    operandOne: [],
+    operator: "",
+    operandTwo: [],
+};
+
+const buttons = document.querySelector(".buttons");
+buttons.addEventListener("click", (event) => {
+    let target = event.target.closest(".btn") || event.target.closest(".btn-zero")
+
+    const display = document.querySelector(".display");
+
+
+    if (target.classList.contains("number")) {
+        numberPressed(calculator, target, display);
+    } else if (target.classList.contains("operator")) {
+        operatorPressed(calculator, target, display);
+    } else if (target.classList.contains("special")) {
+        specialPressed(calculator, target, display);
+    }
+})
+
 function add(a, b) {
     return a + b;
 }
@@ -43,75 +70,98 @@ function operate(a, b, operator) {
     }
 }
 
-const disp = document.querySelector(".display");
-disp.textContent = "";
-
-let a = "";
-console.log(a);
-let b = "";
-let operator = "";
-let equals = false;
-const buttons = document.querySelector(".buttons");
-buttons.addEventListener("click", (event) => {
-    let target = event.target.closest(".btn") || event.target.closest(".btn-zero")
-
-    const display = document.querySelector(".display");
 
 
-    if (target.classList.contains("number")) {
-        if (operator === "") {
-            let newDigit = target.textContent;
-            console.log(target.textContent)
-            display.textContent += newDigit;
-            a += newDigit;
-        } else {
-            b += target.textContent;
-            let newDigit = target.textContent;
-            display.textContent += newDigit;    
+function numberPressed(calculator, target, display) {
+    let newDigit = target.textContent; // we take the content of the button pressed as the new digit to register
+    let accepted;
+    if (calculator.operator === "") { // operator has not been pressed yet -> we are editing operandTwo
+        accepted = processNewDigit(calculator.operandOne, newDigit) // we add the new digit to operandOne
+    } else { // operator has been pressed -> we are processing operandTwo
+        if (display.textContent === calculator.operandOne.join("") && calculator.operandTwo.length === 0) { // we are currently displaying the result of a previous chain
+            display.textContent = ""; // start fresh for operandTwo 
         }
-    } else if (target.classList.contains("operator")) {
-        let op = target.textContent;
+        accepted = processNewDigit(calculator.operandTwo, newDigit) // append new digit to operandTwo 
+    }
+    if (accepted) {
+        display.textContent += newDigit;
+    }
+}
 
-        if (op !== "=") {
-            if (operator !== "") { // if we already have an operator -> do chained calculation
-                a = operate(a, b, operator);
-                display.textContent = `${a}`;
-                b = ""; 
-                operator = "";
-            } else {
-                operator = op;
-                display.textContent = "";
-            }
-        } else {
-            display.textContent = "";
-            let result = operate(a, b, operator);
-            display.textContent = `${result}`;
-            a = result.toString(); // keep current result as first operand of next calculation
-            b = "";
-            operator = ""
-        }
-    } else if (target.classList.contains("special")) {
-        if (target.textContent === "AC") {
-            display.textContent = "";
-            a = "";
-            b = "";
-            operator = "";
-            equals = false;
-        } else if (target.textContent === "DEL") {
-            if (display.textContent !== "") {
-                if (a !== "" && b === "") { // if we have a but not b
-                    a = a.slice(0, -1); // remove last element
-                    display.textContent = a;
-                } else {
-                    b = b.slice(0, -1);
-                    display.textContent = b;
-                }
-            }
-        } else if (target.textContent === "%") {
-            operator = "%";
-            display.textContent = "";
-
+function processNewDigit(operand, newDigit) {
+    if (newDigit === ".") {
+        if (operand.includes(newDigit)) {
+            return false;
+        } else if (operand.length === 0) {
+            operand.push("0");
         }
     }
-})
+    operand.push(newDigit);
+    return true;
+}
 
+function operatorPressed(calculator, target, display) {
+    let op = target.textContent; // the operator that was just pressed
+
+    if (op !== "=") { // the operator is not =
+        if (calculator.operator === "" &&  // operator had not been pressed yet in current chain
+            calculator.operandOne.length !== 0 && // operandOne has been entered
+            calculator.operandTwo.length === 0) {  // operandTwo is yet to be entered
+                display.textContent = ""; // clear display
+                calculator.operator = op;
+        } else if (calculator.operator !== "" && // operator has been entered before
+            calculator.operandOne.length !== 0 && // operandOne has been entered
+            calculator.operandTwo.length !== 0) { // operandTwo has been entered 
+                // this is our calculation chaining logic
+                let result = operate(calculator.operandOne.join(""), calculator.operandTwo.join(""), calculator.operator); // complete calculation with previous operator
+                calculator.operandOne = String(result).split(""); // set result of previous chain to the current operandOne
+                calculator.operandTwo.length = 0; // clear operandTwo
+                calculator.operator = op; // set operator to the operator that was just pressed
+
+                display.textContent = calculator.operandOne.join(""); // display operandOne
+        } else if (calculator.operator !== "" && // operator has been entered
+            calculator.operandOne.length !== 0 && // operandOne is populated -> ie during a chain
+            calculator.operandTwo.length === 0 // operandTwo is empty -> it has been used in a calculation to become operandOne
+        ) {
+            // this is the second part of the calculation chaining logic
+            display.textContent = "";
+
+        }
+    } else { // "=" has been pressed
+        if (calculator.operandOne.length !== 0 &&
+            calculator.operator !== "" &&
+            calculator.operandTwo.length !== 0
+        ) { // check if all three params for operate() are valid
+            console.log(calculator);
+            calculator.operandOne = String(operate(calculator.operandOne.join(""), calculator.operandTwo.join(""), calculator.operator)).split(""); // calculate the expression
+            display.textContent = calculator.operandOne.join(""); // push result to display
+            calculator.operandTwo.length = 0;
+            calculator.operator = "";
+        }
+    }
+}
+
+function specialPressed(calculator, target, display) {
+    if (target.textContent === "AC") {
+        clear(calculator, display);
+    } else if (target.textContent === "DEL") {
+        del(calculator, display);
+    }
+}
+
+function clear(calculator, display) {
+    calculator.operandOne.length = 0;
+    calculator.operator = "";
+    calculator.operandTwo.length = 0;
+
+    display.textContent = "";
+}
+
+function del(calculator, display) {
+    if (calculator.operandTwo.length !== 0 && display.textContent === calculator.operandTwo.join("")) { // if operandTwo has digits and is being displayed
+        calculator.operandTwo.splice(calculator.operandTwo.length - 1, 1); // remove last digit from operandTwo
+    } else if (calculator.operandOne.length !== 0 && display.textContent === calculator.operandOne.join("")) { // if operandOne has digits and is being displayed
+        calculator.operandOne.splice(calculator.operandOne.length - 1, 1); // remove last digit from operandTwo
+    }    
+    display.textContent = display.textContent.slice(0, -1); // remove last character from what's being displayed
+}
